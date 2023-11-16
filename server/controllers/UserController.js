6cfff2629272009d79985fc38054166ab33c32b7
arvinaufal <arvinaufalagustian@gmail.com>
2023-11-17 06:31:38 +0700

    add testing for book and history

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -5,49 +5,6 @@ const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client();
 
 class UserController {
-    static async getTopThree(request, response) {
-        try {
-            const users = await User.findAndCountAll({
-                include: [
-                    { model: UserDetail },
-                    {
-                        model: History,
-                        attributes: [
-                            [sequelize.fn('SUM', sequelize.col('point')), 'totalPoints']
-                        ],
-                        group: ['userId'],
-                    },
-                ],
-                order: [
-                    [sequelize.literal('totalPoints DESC')],
-                ],
-                limit: 3,
-            });
-
-            response.status(200).json(users);
-        } catch (error) {
-            next(error);
-        }
-    }
-
-    static async getById(request, response, next) {
-        try {
-            const user = await User.findByPk(request.params.id, {
-                include: [
-                    { model: UserDetail },
-                    { model: History },
-                ]
-
-            });
-
-            if (!user) throw ({ name: "NotFound" });
-
-            response.status(200).json(user);
-        } catch (error) {
-            next(error);
-        }
-    }
-
     static async create(request, response, next) {
         const { username, email, password, role = 'member', accountType = 'manual', name, address, gender } = request.body;
         try {
@@ -55,35 +12,6 @@ class UserController {
             await UserDetail.create({ userId: user.id, name, address, gender });
 
             response.status(201).json({ id: user.id, email: user.email, role: user.role });
-        } catch (error) {
-            next(error);
-        }
-    }
-
-    static async update(request, response, next) {
-        const { username, email, password, name, address, gender } = request.body;
-        try {
-            let user = await User.findByPk(request.params.id, {
-                include: {
-                    model: UserDetail
-                }
-            });
-
-            await user.update({ username, email, password });
-            await user.UserDetail.update({ name, address, gender });
-
-            response.status(200).json(user);
-        } catch (error) {
-            next(error);
-        }
-    }
-
-    static async delete(request, response, next) {
-        try {
-            let user = await User.findByPk(request.params.id);
-
-            await user.destroy();
-            response.status(200).json({ message: `User with id: ${user.id} success to delete` });
         } catch (error) {
             next(error);
         }
@@ -97,7 +25,7 @@ class UserController {
 
             const user = await User.findOne({ where: { email } });
             if (!user) throw ({ name: "NotMatched" });
-            if(user.accountType === 'google') throw ({name: 'googleAcc'});
+            if (user.accountType === 'google') throw ({ name: 'googleAcc' });
             if (!bcryptCompare(password, user.password)) throw ({ name: "NotMatched" });
             response.status(200).json({ access_token: createToken({ id: user.id }), email: user.email, role: user.role });
         } catch (error) {
@@ -125,9 +53,84 @@ class UserController {
             if (newUser) await UserDetail.create({ userId: user.id, name: user.username });
             res.status(newUser ? 201 : 200).json({ access_token: createToken({ id: user.id }) });
         } catch (error) {
+            console.log(error);
             next(error);
         }
     }
 }
 
 module.exports = UserController;
+
+
+// static async update(request, response, next) {
+//     const { username, email, password, name, address, gender } = request.body;
+//     try {
+//         let user = await User.findByPk(request.params.id, {
+//             include: {
+//                 model: UserDetail
+//             }
+//         });
+
+//         await user.update({ username, email, password });
+//         await user.UserDetail.update({ name, address, gender });
+
+//         response.status(200).json(user);
+//     } catch (error) {
+//         next(error);
+//     }
+// }
+
+// static async delete(request, response, next) {
+//     try {
+//         let user = await User.findByPk(request.params.id);
+
+//         await user.destroy();
+//         response.status(200).json({ message: `User with id: ${user.id} success to delete` });
+//     } catch (error) {
+//         next(error);
+//     }
+// }
+
+
+// static async getTopThree(request, response) {
+//     try {
+//         const users = await User.findAndCountAll({
+//             include: [
+//                 { model: UserDetail },
+//                 {
+//                     model: History,
+//                     attributes: [
+//                         [sequelize.fn('SUM', sequelize.col('point')), 'totalPoints']
+//                     ],
+//                     group: ['userId'],
+//                 },
+//             ],
+//             order: [
+//                 [sequelize.literal('totalPoints DESC')],
+//             ],
+//             limit: 3,
+//         });
+
+//         response.status(200).json(users);
+//     } catch (error) {
+//         next(error);
+//     }
+// }
+
+// static async getById(request, response, next) {
+//     try {
+//         const user = await User.findByPk(request.params.id, {
+//             include: [
+//                 { model: UserDetail },
+//                 { model: History },
+//             ]
+
+//         });
+
+//         if (!user) throw ({ name: "NotFound" });
+
+//         response.status(200).json(user);
+//     } catch (error) {
+//         next(error);
+//     }
+// }
