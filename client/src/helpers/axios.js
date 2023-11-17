f8bbb3908fcf2b102fd98107baec8fd5e0194411
arvinaufal <arvinaufalagustian@gmail.com>
2023-11-17 07:41:11 +0700

    Change baseURL from localhost to production url server

--- client/src/helpers/axios.js
+++ client/src/helpers/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: 'http://localhost:3000',
-    // baseURL: 'https://ipustaka-server.arvinaufal.my.id', // deployment / cloud
+    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://ipustaka-server.arvinaufal.my.id', // deployment / cloud
 });
 
 export default request;
